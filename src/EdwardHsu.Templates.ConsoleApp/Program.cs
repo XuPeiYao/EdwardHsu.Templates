cddcaa37596fec0b47507712efed43cd07665a2e
Edward Hsu <st89382000@gmail.com>
2022-03-19 10:20:40 +0800

    feat: Add ConsoleApp template

---
+++ src/EdwardHsu.Templates.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EdwardHsu.Templates.ConsoleApp
+{
+    public class Program
+    {
+        public async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
